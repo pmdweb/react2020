fdc72beb76d91df3a6701ae80ff5c7c99fbec4ec
Pedro M. Duarte <pmdweb@gmail.com>
2022-12-16 20:58:15 -0300

    testing site changes
    Signed-off-by: Pedro M. Duarte <pmdweb@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,11 @@
 import { useState } from "react";
 import { Tweet } from "./componentes/tweets"
 import './App.css'
+import { AppRoutes } from "./Routes";
 
 function App() {
-  const [tweets, setTweets] = useState<string[]>([
-    'tweet 1',
-    'tweet 2',
-    'tweet 3',
-    'tweet 4',
-  ])
-  function createTweet() {
-    setTweets([...tweets,'tweet 5'])
-  }
-
   return (
-    <div>
-      {tweets.map(tweet => {
-        return <Tweet text={tweet } />
-      })}
-
-      <button
-        onClick={createTweet}
-        style={{
-          backgroundColor: "white",
-          border: 0,
-          padding: '6px 12px'
-        }}
-      >
-        add
-      </button>
-    </div>
+    <AppRoutes />
   );
 }
 
